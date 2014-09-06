548cbe7b0293e5348d0ca37813b7d613bbc59e20
Vincent <tqm397@gmail.com>
2014-09-05 23:56:15 -0400

    Fixed dead link in the footer.

--- js/copyright.js
+++ js/copyright.js
@@ -3,6 +3,6 @@ document.write("<font size='2' color='#F3BD01' face='Arial'>Copyright 2007-2011 
 document.write("<font face='Arial' size='3' color='#F3BD01'>");
 document.write("<br/>");
 document.write("<a href='team/aboutus/' style='text-decoration:none;color:#FFFFFF;'>About Us</a> | ");
-document.write("<a href='Contact/' style='text-decoration:none;color:#FFFFFF;'>Contact</a> | ");
-document.write("<a href='http://www.usfirst.org/'style='text-decoration:none;color:#FFFFFF;' target='_blank'>FIRST</a></font>");
+document.write("<a href='contact/' style='text-decoration:none;color:#FFFFFF;'>Contact</a> | ");
+document.write("<a href='http://www.usfirst.org/'style='text-decoration:none;color:#FFFFFF;' target='_blank'><i>FIRST</i></a></font>");
 document.write("</div>");
